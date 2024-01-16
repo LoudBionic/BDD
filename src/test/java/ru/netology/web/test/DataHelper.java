ae50fc0964ff908d3abba17dd6f8054236bcf56c
Gleb <Sephirothash@yandex.ru>
2024-01-16 20:50:43 +0300

    last

--- src/test/java/ru/netology/web/test/DataHelper.java
+++ src/test/java/ru/netology/web/test/DataHelper.java
@@ -18,11 +18,11 @@ public class DataHelper {
 
 
     public static CardInfo getFirstCardInfo() {
-        return new CardInfo("0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+        return new CardInfo("**** **** **** 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
     }
 
     public static CardInfo getSecondCardInfo() {
-        return new CardInfo("0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+        return new CardInfo("**** **** **** 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
 
 
